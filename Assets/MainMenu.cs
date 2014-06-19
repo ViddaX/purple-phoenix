ddb8c39a3918bcfaee2e89b543d7a538f253fdfc
ViddaX <dixon3133@yahoo.com>
2014-06-19 17:35:18 +0100

    main menu scene and compressed components

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace pp2{
+	public class MainMenu : MonoBehaviour {
+
+		void OnGUI() {
+			GUI.Box(new Rect(630,150,110,150), "Main Menu");
+		
+
+		if(GUI.Button(new Rect(640,175,80,20), "Start Game")){
+				Application.LoadLevel(1);
+		}
+
+
+		if(GUI.Button(new Rect(640,200,80,20), "Level 2")) {
+			Application.LoadLevel(2);
+		}
+
+		}
+	}
+}
