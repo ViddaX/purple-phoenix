b8bcbe11ec2f73d9f462066dfb0a7f31c430c0ec
ViddaX <dixon3133@yahoo.com>
2014-06-20 13:36:28 +0100

    main menu

--- Assets/DirectionSelector.cs
+++ Assets/DirectionSelector.cs
@@ -11,24 +11,28 @@ namespace pp{
 			GameObject obj = GameObject.Find ("Plane");
 			Grid grid =	obj.GetComponent<Grid>();
 			GUI.skin = Skin;
-			GUI.Box(new Rect(30, top,150,130), "Direction");
 
-			if(GUI.Button(new Rect(80, top + 30,60,30), "Up")) {
-				grid.direction = Direction.WEST;
+			if (BuildingMenu.inGame == true) {
+
+								GUI.Box (new Rect (30, top, 150, 130), "Direction");
+
+								if (GUI.Button (new Rect (80, top + 30, 60, 30), "Up")) {
+										grid.direction = Direction.WEST;
 				
-			}
+								}
 
-			if(GUI.Button(new Rect(35, top + 60,60,30), "Left")) {
-				grid.direction = Direction.SOUTH;
-			}
+								if (GUI.Button (new Rect (35, top + 60, 60, 30), "Left")) {
+										grid.direction = Direction.SOUTH;
+								}
 
-			if(GUI.Button(new Rect(120, top + 60,60,30), "Right")) {
-				grid.direction = Direction.NORTH;
-			}
+								if (GUI.Button (new Rect (120, top + 60, 60, 30), "Right")) {
+										grid.direction = Direction.NORTH;
+								}
 			
-			if(GUI.Button(new Rect(80, top + 90,60,30), "Down")) {
-				grid.direction = Direction.EAST;
-			}
+								if (GUI.Button (new Rect (80, top + 90, 60, 30), "Down")) {
+										grid.direction = Direction.EAST;
+								}
+						}
 		}
 	
 	
